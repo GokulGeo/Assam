9ec79de2728c420acff4bc99710bc62a28cfc4c6
GokulGeo <gokulanand03@gmail.com>
2020-05-13 16:14:37 +0530

    latest

---
+++ Subansiri.R
@@ -0,0 +1,69 @@
+library(rayshader)
+library(sp)
+library(raster)
+library(scales)
+library(rgdal)
+
+#Here, I load a map with the raster package.
+#loadzip = tempfile() 
+#download.file("https://tylermw.com/data/dem_01.tif.zip", loadzip)
+#localtif = raster::raster(unzip(loadzip, "dem_01.tif"))
+#unlink(loadzip)
+
+localtif <- raster("C:\\Users\\Lenovo\\Desktop\\Cover\\Additional data\\cdng46e_v3r1\\DEM_prj.tif")
+
+#And convert it to a matrix:
+elmat = raster_to_matrix(localtif)
+
+#We use another one of rayshader's built-in textures:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  plot_map()
+
+#sphere_shade can shift the sun direction:
+elmat %>%
+  sphere_shade(sunangle = 174, texture = "desert") %>%
+  plot_map()
+
+#detect_water and add_water adds a water layer to the map:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  plot_map()
+
+#And we can add a raytraced layer from that sun direction as well:
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat), 0.5) %>%
+  plot_map()
+
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat), 0.5) %>%
+  add_shadow(ambient_shade(elmat), 0) %>%
+  plot_map()
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  add_shadow(ray_shade(elmat, zscale = 3), 0.5) %>%
+  add_shadow(ambient_shade(elmat), 0) %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 135, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
+Sys.sleep(0.2)
+render_snapshot()
+
+elmat %>%
+  sphere_shade(texture = "desert") %>%
+  add_water(detect_water(elmat), color = "desert") %>%
+  plot_3d(elmat, zscale = 10, fov = 0, theta = 60, zoom = 0.75, phi = 45, windowsize = c(1000, 800))
+
+render_scalebar(limits=c(0, 5, 10),label_unit = "km",position = "W", y=50,
+                scale_length = c(0.33,1))
+
+render_compass(position = "E")
+Sys.sleep(0.2)
+render_highquality(samples=200, scale_text_size = 24,clear=TRUE)
+
